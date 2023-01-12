01b22137732c5fc3d6ffafcadc60295ac4bce625
ambi hidalgo <ambio.pk@gmail.com>
2023-01-11 20:49:01 -0800

    WIP Refactor makeApiRequest
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>
    Co-authored-by: Ariel Doyle  <ariel.rose.garrett@gmail.com> 

--- src/index.js
+++ src/index.js
@@ -10,44 +10,128 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+async function makeApiRequest(url = 'https://api.openai.com/v1/completions', payload) {
 
-
-function makeApiRequest(artist1, artist2, artist3) {
-  let promise = new Promise(function(resolve, reject) {
-    let xml = new XMLHttpRequest();
-    xml.addEventListener("loadend", () => {
-      const response = JSON.parse(xml.response);
-      // console.log(response.choices[0].text);
-      if (xml.status === 200) {
-        resolve([response, artist1, artist2, artist3]);
-      } else {
-        reject([response, artist1, artist2, artist3])
-      }
-    });
-    xml.open("POST", "https://api.openai.com/v1/completions");
-    xml.setRequestHeader("Content-Type", "application/json");
-    xml.setRequestHeader("Authorization", `Bearer ${process.env.OPENAI_API_KEY}`);
-    
-    const payload = {
-      model: "text-davinci-003",
-      prompt: `Give me a list of 5 recommended musical artists based off of my top 3 picks of ${artist1}, ${artist2}, ${artist3}\n`,
-      temperature: 1.2,
-      max_tokens: 100,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      stop: [" Human:", " AI:"],
-    };
-    console.log(xml);
-    xml.send(JSON.stringify(payload));
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json',
+      'Authorization':`Bearer ${process.env.OPENAI_API_KEY}`
+    },
+    body: JSON.stringify(payload)  
   });
-  debugger;
-  promise.then(function(response){
-    printElements(response);
-  }, function(response){
-    printError(response);
+  return response.json();
+}
+
+
+function handleSubmit(event){
+  event.preventDefault();
+  let artist1 = document.getElementById("artist1").value;
+  let artist2 = document.getElementById("artist2").value;
+  let artist3 = document.getElementById("artist3").value;
+  const payload = {
+    model: "text-davinci-003",
+    prompt: `Give me a list of 5 recommended musical artists based off of my top 3 picks of ${artist1}, ${artist2}, ${artist3}\n`,
+    temperature: 1.2,
+    max_tokens: 100,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop: [" Human:", " AI:"],
+  };  
+  const url = 'https://api.openai.com/v1/completions'
+
+  makeApiRequest(url, payload)
+  .then(function(response) {
+    if (!response.ok) {
+      const errorMessage = `${response.status} ${response.statusText}`;  
+      throw new Error(errorMessage);
+    } else {
+      return response.json();
+    }
+  })
+  .then(function(jsonResponse) {
+    printElements(jsonResponse);
+    console.log(jsonResponse);
+  })
+  .catch(function(error) {
+    printError(error);
   });
 }
+
+
+
+// const data = { username: 'example' };
+
+// fetch('https://example.com/profile', data).then((response) => response.json()).then((data) => {
+//     console.log('Success:', data);
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error);
+//   });
+
+
+
+
+
+
+
+
+function printElements(response){
+  document.getElementById("showResponse").innerText = `${response[0].choices[0].text}`
+}
+function printError(response){
+
+  document.getElementById("showResponse").innerText = `There was an error: ${response[0].error.message}`;
+  console.log(response[0].error.message);
+}
+
+window.addEventListener("load", function() {
+
+  document.querySelector('form').addEventListener("submit", handleSubmit);
+});
+
+
+
+
+
+// function makeApiRequest(artist1, artist2, artist3) {
+  //   let promise = new Promise(function(resolve, reject) {
+//     debugger;
+//     let xml = new XMLHttpRequest();
+//     xml.addEventListener("loadend", () => {
+//       const response = JSON.parse(xml.response);
+//       // console.log(response.choices[0].text);
+//       if (xml.status === 200) {
+//         resolve([response, artist1, artist2, artist3]);
+//       } else {
+//         reject([response, artist1, artist2, artist3])
+//       }
+//     });
+//     xml.open("POST", "https://api.openai.com/v1/completions");
+//     xml.setRequestHeader("Content-Type", "application/json");
+//     xml.setRequestHeader("Authorization", `Bearer ${process.env.OPENAI_API_KEY}`);
+    
+//     const payload = {
+//       model: "text-davinci-003",
+//       prompt: `Give me a list of 5 recommended musical artists based off of my top 3 picks of ${artist1}, ${artist2}, ${artist3}\n`,
+//       temperature: 1.2,
+//       max_tokens: 100,
+//       top_p: 1,
+//       frequency_penalty: 0,
+//       presence_penalty: 0,
+//       stop: [" Human:", " AI:"],
+//     };
+//     console.log(xml);
+//     xml.send(JSON.stringify(payload));
+//   });
+
+//   promise.then(function(response){
+//     printElements(response);
+//   }, function(response){
+//     printError(response);
+//   });
+// }
 
 // function makeApiRequest(artist1, artist2, artist3) {
 //   const xml = new XMLHttpRequest();
@@ -72,26 +156,6 @@ function makeApiRequest(artist1, artist2, artist3) {
 //     console.log(response.choices[0].text);
 //   });
 // }
-function printElements(response){
-  document.getElementById("showResponse").innerText = `${response[0].choices[0].text}`
-}
-function printError(response){
-
-  document.getElementById("showResponse").innerText = `There was an error: ${response[0].error.message}`;
-  console.log(response[0].error.message);
-}
-function handleSubmit(event){
-  event.preventDefault();
-  let artist1 = document.getElementById("artist1").value;
-  let artist2 = document.getElementById("artist2").value;
-  let artist3 = document.getElementById("artist3").value;
-  makeApiRequest(artist1, artist2, artist3);
-  
-}
-window.addEventListener("load", function() {
-
-  document.querySelector('form').addEventListener("submit", handleSubmit);
-});
 // function handleSubmit(event){
 //   event.preventDefault();
 //   try{
